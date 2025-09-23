2b44193124640a94278ca3a3b36fd5c2f22b6367
pet2016 <18022887432@163.com>
2025-09-23 12:04:28 +0800

    add project

---
+++ src/test/java/com/sanyao/ai/deepseek/SpringDeepseekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanyao.ai.deepseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeepseekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
